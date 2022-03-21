ec4b8ad224d38418ed4f60f8a59559a92d4bb012
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-03-21 17:26:33 -0300

    [feat]: adicionou helpers para leitura e gravao em arquivo json

---
+++ helpers/readWriteFile.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises;
+
+const readContentFile = async (path) => {
+  try {
+    const content = await fs.readFile(path, 'utf8');
+    return JSON.parse(content);
+  } catch (error) {
+    return null;
+  }
+};
+
+const writeContentFile = async (path, content) => {
+  try {
+    const arrContent = await readContentFile(path);
+
+    arrContent.push(content);
+    await fs.writeFile(path, JSON.stringify(arrContent));
+
+    return content;
+  } catch (error) {
+    return null;
+  }
+};
+
+module.exports = {
+  readContentFile,
+  writeContentFile,
+};
+
+// Retirado do Course - atividade 4 do bloco 22 de back-end
