4d124de42faaba491ceace91739015ea58e016ef
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-03-21 19:42:40 -0300

    [feat]: adicionou middleware para validacao de talker criado - requisito 4

---
+++ middlewares/createTalkerValidation.js
@@ -0,0 +1,76 @@
+const isValidTalkerName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' }); 
+  }
+
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' }); 
+  }
+
+  next();
+};
+
+const isValidTalkerAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' }); 
+  }
+
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' }); 
+  }
+  next();
+};
+
+const isValidTalkerTalkDate = (req, res, next) => {
+  const { talk } = req.body;
+  const validDateRegex = /^[0-9]{2}[/]{1}[0-9]{2}[/]{1}[0-9]{4}$/g;
+
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' }); 
+  }
+  if (!talk.watchedAt.match(validDateRegex)) {
+    return res.status(400)
+    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' }); 
+  }
+
+  next();
+};
+
+const isValidTalkerTalkRate = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (talk.rate < 1 || talk.rate > 5) {
+    return res.status(400)
+    .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' }); 
+  }
+
+  next();
+};
+
+const isValidToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  const tokenLength = 16;
+  
+  if (!token) { 
+    return res.status(401).json({ message: 'Token não encontrado' }); 
+  }
+
+  if (token.length !== tokenLength) { 
+    return res.status(401).json({ message: 'Token inválido' }); 
+  }
+  
+    next();
+  };
+
+module.exports = {
+  isValidTalkerName,
+  isValidTalkerAge,
+  isValidTalkerTalkDate,
+  isValidTalkerTalkRate,
+  isValidToken,
+};
