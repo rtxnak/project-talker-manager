d1ca77ef18da12eee6b2418807dc27dbec500277
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-03-21 17:47:55 -0300

    [feat]: adicionou metodo de requisicao GET para rota por id => /talkers/:id - requisito 2

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -11,4 +11,14 @@ router.get('/talker', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+router.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readContentFile(PATH_FILE) || [];
+  const talkerID = talkers.find((talker) => talker.id === parseInt(id, 10));
+
+    if (!talkerID) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talkerID);
+});
+
 module.exports = router;
