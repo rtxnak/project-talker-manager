9698d2b03f81c63d587b8e8935b53890ed050300
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-03-22 18:37:37 -0300

    [feat]: adicionou ENDPOINT Delete para rota talker/:id

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -91,4 +91,24 @@ router.put(
   },
 );
 
+router.delete(
+  '/talker/:id',
+  isValidToken,
+  async (req, res) => {
+    const { id } = req.params;
+    const talkers = await readContentFile(PATH_FILE) || [];
+    const talkersIndex = talkers.findIndex((talker) => talker.id === parseInt(id, 10));
+
+    if (talkersIndex === -1) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+
+    talkers.splice(talkersIndex, 1);
+    
+    await writeContentFile(PATH_FILE, talkers);
+
+    return res.status(204).end();
+  },
+);
+
 module.exports = router;
